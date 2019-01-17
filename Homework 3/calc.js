607e268c818905b1efa42f394fbe4eb4c3d7d67a
Maciej Perdenia <maciej.perdenia@gmail.com>
2019-01-17 15:25:06 +0100

    [H3] Implement the calculator functionality

--- Homework 3/calc.js
+++ Homework 3/calc.js
@@ -1,3 +1,103 @@
-/*
- * Implement all your JavaScript in this file!
- */
+//TODO: figure out when to reset operation and input
+'use strict';
+
+var result = null;
+var input = null;
+var operation = '';
+var display = '';
+
+var equalsClicked = false;
+var lastInput = null;
+var lastOperation = '';
+
+$('.digitButton').click(function () {
+  equalsClicked = false;
+  if (operation == '') {
+    result = null;
+  }
+  if (display == '0') {
+    display = '';
+  }
+  display = display.concat($(this).val());
+  input = Number(display);
+  $('#display').val(display);
+  console.log('DIG/ result: ' + result + ', input: ' + input +
+    ', operation: ' + operation + ', display: ' + display +
+    ', lastInput: ' + lastInput + ', lastOperation: ' + lastOperation +
+    ', equalsClicked: ' + equalsClicked);
+});
+
+$('.operationButton').click(function () {
+  equalsClicked = false;
+  if (display != '') {
+    equals(input, operation);
+  }
+  input = null;
+  operation = $(this).attr('name');
+  console.log('OPE/ result: ' + result + ', input: ' + input +
+    ', operation: ' + operation + ', display: ' + display +
+    ', lastInput: ' + lastInput + ', lastOperation: ' + lastOperation +
+    ', equalsClicked: ' + equalsClicked);
+});
+
+$('#equalsButton').click(function () {
+  if (equalsClicked && lastInput != null && lastOperation != '') {
+    equals(lastInput, lastOperation);
+  } else if (input != null) {
+    equals(input, operation);
+    equalsClicked = true;
+  }
+  input = null;
+  operation = '';
+  console.log('EQB/ result: ' + result + ', input: ' + input +
+    ', operation: ' + operation + ', display: ' + display +
+    ', lastInput: ' + lastInput + ', lastOperation: ' + lastOperation +
+    ', equalsClicked: ' + equalsClicked);
+});
+
+$('#clearButton').click(function () {
+  equalsClicked = false;
+  result = null;
+  input = null;
+  lastInput = null;
+  operation = '';
+  lastOperation = '';
+  display = '';
+  $('#display').val(display);
+  console.log('CLE/ result: ' + result + ', input: ' + input +
+    ', operation: ' + operation + ', display: ' + display +
+    ', lastInput: ' + lastInput + ', lastOperation: ' + lastOperation +
+    ', equalsClicked: ' + equalsClicked);
+});
+
+function equals(input, operation) {
+  switch (operation) {
+    case 'add':
+      result += input;
+      break;
+    case 'subtract':
+      result -= input;
+      break;
+    case 'multiply':
+      result *= input;
+      break;
+    case 'divide':
+      result /= input;
+      break;
+    default:
+      if (result == null) {
+        result = input;
+      }
+      // console.log('Operation unknown');
+      break;
+  }
+  lastInput = input;
+  lastOperation = operation;
+  display = result.toString();
+  $('#display').val(display);
+  display = '';
+  console.log('EQU/ result: ' + result + ', input: ' + window.input +
+    ', operation: ' + window.operation + ', display: ' + display +
+    ', lastInput: ' + lastInput + ', lastOperation: ' + lastOperation +
+    ', equalsClicked: ' + equalsClicked);
+}
